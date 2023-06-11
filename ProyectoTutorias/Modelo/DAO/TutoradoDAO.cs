395cf4c0d48d7cc715a451eb2b4802f9735a290e
BenjaminGarcia754 <linkdelira@gmail.com>
2023-06-11 15:17:48 -0600

    Ejemplo

--- ProyectoTutorias/Modelo/DAO/TutoradoDAO.cs
+++ ProyectoTutorias/Modelo/DAO/TutoradoDAO.cs
@@ -33,5 +33,36 @@ namespace ProyectoTutorias.Modelo.DAO
             }
             return respuesta;
         }
+
+        public static bool EditarEstudiante(TutoradoPOJO nuevoTutorado)
+        {
+            bool respuesta;
+            var conexionBD = Conexion.GenerarConexion();
+
+            try
+            {
+                Tutorado tutorado = (from tutoradoQuery in conexionBD.Tutorado
+                                     where tutoradoQuery.idTutorado == nuevoTutorado.idTutorado
+                                     select tutoradoQuery).FirstOrDefault();
+
+                tutorado.idProgramaEducativo = nuevoTutorado.idProgramaEducativo;
+                tutorado.tutorAcademico = nuevoTutorado.tutorAcademico;
+                tutorado.estaActivo = nuevoTutorado.estaActivo;
+                tutorado.estaEnRiesgo = nuevoTutorado.estaEnRiesgo;
+                tutorado.nombre = nuevoTutorado.nombre;
+                tutorado.matricula = nuevoTutorado.matricula;
+                tutorado.telefono = nuevoTutorado.telefono;
+                tutorado.numeroCambiosTutor = nuevoTutorado.numeroCambiosTutor;
+                conexionBD.SubmitChanges();
+                respuesta = true;
+            }
+            catch (Exception)
+            {
+
+                respuesta = false;
+            }
+            
+            return respuesta;
+        }
     }
 }
